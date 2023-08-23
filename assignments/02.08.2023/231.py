08a62e1e07a3f9ac21087c9911d37128237afae9
Parshwa Shah <parshwacshah@gmail.com>
2023-08-23 14:52:08 +0530

    parshwa

--- assignments/02.08.2023/231.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        def pow(n):
-            if n==1:
-                return True
-            if n==0 or n%2!=0:
-                return False
-            return pow(n/2)
-        return pow(n)
